83ee7efd379e8ea00808394e8c5056917c6e490b
zzxiexin <344391941@qq.com>
2024-02-22 14:55:47 +0800

    feat: 去除无用代码

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
-import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 import { jwtConstants } from './constants';
 
